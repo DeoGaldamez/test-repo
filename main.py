8c150e5d2fe0527a6e361ccc9dca3b6abedc216d
DeoGaldamez <71427577+DeoGaldamez@users.noreply.github.com>
2024-07-08 10:33:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -19,5 +19,5 @@ def hello_http(request):
     elif request_args and 'name' in request_args:
         name = request_args['name']
     else:
-        name = 'Dieguillo :D, funcionno la repo!!!'
+        name = 'Deonel :D, funcionno la repo manual!!!'
     return 'Hellooooooooooo {}!'.format(name)
